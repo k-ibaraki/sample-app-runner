1a4a00296c8f6139d7f15e26b60d6d56faf9e2b5
k-ibaraki <43743845+k-ibaraki@users.noreply.github.com>
2021-05-21 16:46:33 +0900

    CDのテスト

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 // Get
 const router: express.Router = express.Router()
 router.get('/', (req: express.Request, res: express.Response) => {
-  res.send("Hello World")
+  res.send("Hello World 2")
 })
 app.use(router)
 
