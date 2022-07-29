07bbb55ee5259e066e4aa093d4c793aaafc8a5c2
k-ibaraki <43743845+k-ibaraki@users.noreply.github.com>
2022-07-29 19:31:52 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 // Get
 const router: express.Router = express.Router()
 router.get('/', (req: express.Request, res: express.Response) => {
-  res.send("Hello World ３")
+  res.send("Hello World 4")
 })
 app.use(router)
 
