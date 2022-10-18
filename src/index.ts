f9082e937e277b1d622f16b91179e40cc66c0767
k-ibaraki <ibaraki@ncdc.co.jp>
2022-10-18 17:47:39 +0900

    express-oas-generatorの導入

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import expressOasGenerator from 'express-oas-generator'
 
 const app: express.Express = express()
 
@@ -18,7 +19,13 @@ const router: express.Router = express.Router()
 router.get('/', (req: express.Request, res: express.Response) => {
   res.send("Hello World 4")
 })
+router.get('/hello', (req: express.Request, res: express.Response) => {
+  res.send("hello world ")
+})
 app.use(router)
+
+// express-oas-generator : OpenAPIドキュメントの自動生成
+expressOasGenerator.init(app, {})
 
 // 3000番ポートでAPIサーバ起動
 app.listen(3000, () => { console.log('Example app listening on port 3000!') })
